5ccdb7c37e99d6c0ddaa1ffbb87160649a7ef16a
bk <bk123yt@gmail.com>
2021-03-11 04:41:24 -0600

    Added variable protection radius based on amount of wealth stashed in one place

--- src/net/pgfmc/balance/events/EntityEvents.java
+++ src/net/pgfmc/balance/events/EntityEvents.java
@@ -18,9 +18,11 @@ public class EntityEvents implements Listener {
 	{
 		
 		Location loc = e.getLocation();
-		int r = 25; // Radius to check for
+		int r = 128; // Radius to check for - should be the max radius for protected land
+		
 		List<Material> mat = Arrays.asList(Material.LAPIS_BLOCK, Material.GOLD_BLOCK, Material.IRON_BLOCK, Material.COAL_BLOCK, Material.OBSIDIAN, Material.CRYING_OBSIDIAN, Material.DIAMOND_BLOCK, Material.EMERALD_BLOCK, Material.BEACON, Material.ENCHANTING_TABLE, Material.ANVIL, Material.REDSTONE_BLOCK, Material.QUARTZ_BLOCK, Material.PACKED_ICE, Material.BLUE_ICE, Material.CONDUIT, Material.NETHERITE_BLOCK, Material.LODESTONE, Material.RESPAWN_ANCHOR, Material.SKELETON_SKULL, Material.SKELETON_WALL_SKULL, Material.WITHER_SKELETON_SKULL, Material.WITHER_SKELETON_WALL_SKULL, Material.CREEPER_HEAD, Material.CREEPER_WALL_HEAD, Material.DRAGON_HEAD, Material.DRAGON_WALL_HEAD, Material.ZOMBIE_HEAD, Material.ZOMBIE_WALL_HEAD, Material.COAL_BLOCK, Material.ENDER_CHEST); // Material of blocks to check for
-		List<Block> blocks = new ArrayList<Block>(); // List to add all blocks to in radius of entity explosion
+		List<Double> wealthPercent = Arrays.asList(0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.500, .50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50);
+		List<Block> blocks = new ArrayList<>(); // List to add all blocks to in radius of entity explosion
 	        
         for (int x = loc.getBlockX() - r; x <= loc.getBlockX() + r; x++)
         {
@@ -28,24 +30,104 @@ public class EntityEvents implements Listener {
             {
                 for (int z = loc.getBlockZ() - r; z <= loc.getBlockZ() + r; z++)
                 {
-                   blocks.add(loc.getWorld().getBlockAt(x, y, z));
+                	if (mat.contains(loc.getWorld().getBlockAt(x, y, z).getType()))
+                	{
+                		blocks.add(loc.getWorld().getBlockAt(x, y, z));
+                	}
                 }
             }
         }
-	    
-	        
-	    for (Block block : blocks) // For every Block in List blocks
-	    {
-	    	for (Material m : mat) // For every Material in List mat
-	    	{
-	    		if (block.getType().equals(m)) // If the type of block equals one of that we're checking for
-	    		{
-	    			e.setCancelled(true);
-	    			return;
-	    		}
-	    	}
-	    	
-	    }
+        
+        
+        
+        int rBlock = 10; // How close should the wealth be to each other?
+        List<Block> protBlocks = new ArrayList<>(); // List to add all blocks to in radius of entity explosion
+        List<Object> radiusHolder = new ArrayList<>();
+        List<List<Object>> totalRadiusHolder = new ArrayList<>(); // Holds lists of information for each block checked - used later to determine the max totalRadius to determine if entity should explode
+        
+        Location blockLoc = null;
+        Location bLoc = null;
+        int n = 0;
+        int totalRadius = 0;
+        double wealth = 0.0;
+        int finalRadius = 0;
+        
+        for (Block block : blocks)
+        {
+        	blockLoc = block.getLocation();
+        	
+        	for (Block b : blocks)
+        	{
+        		bLoc = b.getLocation();
+        		
+                for (int x = blockLoc.getBlockX() - rBlock; x <= blockLoc.getBlockX() + rBlock; x++)
+                {
+                    for (int y = blockLoc.getBlockY() - rBlock; y <= blockLoc.getBlockY() + rBlock; y++)
+                    {
+                        for (int z = blockLoc.getBlockZ() - rBlock; z <= blockLoc.getBlockZ() + rBlock; z++)
+                        {
+                        	if (mat.contains(loc.getWorld().getBlockAt(x, y, z).getType()))
+                        	{
+                        		protBlocks.add(loc.getWorld().getBlockAt(x, y, z));
+                        	}
+                        }
+                    }
+                }
+                
+                for (Material m : mat)
+                {
+                    for (Block pb : protBlocks)
+                    {
+                    	if (pb.getType().equals(m))
+                    	{
+                    		n++;
+                    	}
+                    }
+                    
+                	wealth = wealthPercent.get(mat.indexOf(m));
+                	totalRadius += (wealth * 10) * (Math.sqrt(n * wealth));
+                	n = 0;
+                }
+
+                
+                // Save the variables before reset
+                radiusHolder.add(b);
+                radiusHolder.add(totalRadius);
+                totalRadiusHolder.add(radiusHolder);
+                
+                // Reset the variables for next use
+                totalRadius = 0;
+                protBlocks.clear();
+                radiusHolder.clear();
+                wealth = 0.0;
+                n = 0;
+        	}
+        	
+        	finalRadius = (int) totalRadiusHolder.get(blocks.indexOf(block)).get(1);
+        	
+            for (int x = loc.getBlockX() - finalRadius; x <= loc.getBlockX() + finalRadius; x++)
+            {
+                for (int y = loc.getBlockY() - finalRadius; y <= loc.getBlockY() + finalRadius; y++)
+                {
+                    for (int z = loc.getBlockZ() - finalRadius; z <= loc.getBlockZ() + finalRadius; z++)
+                    {
+                    	if (mat.contains(loc.getWorld().getBlockAt(x, y, z).getType()))
+                    	{
+                    		e.setCancelled(true);
+                    		return;
+                    	}
+                    }
+                }
+            }
+            
+        	
+        }
+        
+        
+
+        
+        
+        
 	}
 
 }
