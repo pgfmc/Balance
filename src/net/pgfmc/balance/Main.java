3e17bbe3e21ccfc032b3eb4f538b607b07a89370
bk <bk123yt@gmail.com>
2021-03-12 15:08:21 -0600

    rrr

--- src/net/pgfmc/balance/Main.java
+++ src/net/pgfmc/balance/Main.java
@@ -1,9 +1,5 @@
 package net.pgfmc.balance;
 
-import java.io.File;
-
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.pgfmc.balance.events.EntityEvents;
@@ -13,8 +9,8 @@ public class Main extends JavaPlugin {
 	
 	public static Main plugin;
 	
-	File file = new File(getDataFolder() + File.separator + "database.yml"); // Creates a File object
-	FileConfiguration database = YamlConfiguration.loadConfiguration(file); // Turns the File object into YAML and loads data
+	// File file = new File(getDataFolder() + File.separator + "database.yml"); // Creates a File object
+	// FileConfiguration database = YamlConfiguration.loadConfiguration(file); // Turns the File object into YAML and loads data
 	
 	@Override
 	public void onEnable()
